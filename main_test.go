7fc9dfa28b9326c63a279d578f0b3c55be16c696
Kien Nguyen <kiennt65@viettel.com.vn>
2019-03-07 15:24:16 +0700

    Remove old version

--- main_test.go
+++ main_test.go
@@ -1,10 +1,3 @@
 package main
 
-import "net/http"
-
-var (
-	_ http.Handler = http.HandlerFunc((&controller{}).index)
-	_ http.Handler = http.HandlerFunc((&controller{}).healthz)
-	_ middleware   = (&controller{}).logging
-	_ middleware   = (&controller{}).tracing
-)
+// Placeholder here
