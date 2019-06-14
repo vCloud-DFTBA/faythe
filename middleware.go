f840c5f7715c8b34523fb65bb968683570441903
Kien Nguyen <kiennt65@viettel.com.vn>
2019-06-14 08:52:36 +0700

    Rework middleware(s)
    * Use Gorrila/mux middleware feature.
    * Easy to modify middlewares as well as extend number of middlewares.

--- middleware.go
+++ middleware.go
@@ -7,42 +7,41 @@ import (
 	"time"
 )
 
-type middleware func(http.Handler) http.Handler
-
 type key int
 
 const (
 	requestIDKey key = 0
 )
 
+type middleware struct {
+	logger        *log.Logger
+	nextRequestID func() string
+}
+
 // logging logs all requests with its information and the time it took to process
-func logging(logger *log.Logger) middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				requestID, ok := r.Context().Value(requestIDKey).(string)
-				if !ok {
-					requestID = "unknown"
-				}
-				start := time.Now()
-				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent(), time.Since(start))
-			}()
-			next.ServeHTTP(w, r)
-		})
-	}
+func (mw *middleware) logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			requestID, ok := r.Context().Value(requestIDKey).(string)
+			if !ok {
+				requestID = "unknown"
+			}
+			start := time.Now()
+			mw.logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent(), time.Since(start))
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
 
 // tracing appends a ID to each request
-func tracing(nextRequestID func() string) middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := r.Header.Get("X-Request-Id")
-			if requestID == "" {
-				requestID = nextRequestID()
-			}
-			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
-			w.Header().Set("X-Request-Id", requestID)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+func (mw *middleware) tracing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestID := r.Header.Get("X-Request-Id")
+		if requestID == "" {
+			requestID = mw.nextRequestID()
+		}
+		ctx := context.WithValue(r.Context(), requestIDKey, requestID)
+		w.Header().Set("X-Request-Id", requestID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
