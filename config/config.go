45a2374e164d8a6709ed6e0b69cd92b867167618
Kien Nguyen <kiennt65@viettel.com.vn>
2019-06-14 10:10:19 +0700

    Restrict domain/subdomain and port as well
    Use the buit-in gorilla/mux feature.
    Close #11.

--- config/config.go
+++ config/config.go
@@ -4,8 +4,14 @@ import "github.com/spf13/viper"
 
 // GlobalConfig represents all configurations.
 type GlobalConfig struct {
-	OpenStack  OpenStackConfiguration
+	OenStack   OpenStackConfiguration
 	StackStorm StackStormConfiguration
+	// RestrictedDomain can define an optional regexp pattern to be matched:
+	//
+	// - {name} matches anything until the next dot.
+	//
+	// - {name:pattern} matches the given regexp pattern.
+	RestrictedDomain string `yaml:"restrictedDomain"`
 }
 
 // Load generates a configuration instance which will be passed around the codebase.
@@ -20,6 +26,8 @@ func Load(cp string) error {
 
 	// Set default values
 	viper.SetDefault("openstack.updateInterval", 30)
+	// Allows all - not restrict any domains
+	viper.SetDefault("restrictedDomain", "{domain:.*}")
 	var cfg GlobalConfig
 	err = viper.Unmarshal(&cfg)
 	return err
