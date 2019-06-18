b6837eac4e2c89e6b34c7e3630b905169c40bd2e
Kien Nguyen <kiennt65@viettel.com.vn>
2019-06-18 17:32:40 +0700

    Handle logging for stackstorm basic case
    Close issue #14.

--- handlers/stackstorm/init.go
+++ handlers/stackstorm/init.go
@@ -22,11 +22,15 @@ func init() {
 }
 
 func forwardReq(r *http.Request, url, apiKey string, body []byte, httpClient *http.Client, wg *sync.WaitGroup) {
-	proxyReq, err := http.NewRequest(r.Method, url, bytes.NewBuffer(body))
+	host := r.RemoteAddr
 	var bodymap template.Alert
-	_ = json.Unmarshal(body, &bodymap)
+	err := json.Unmarshal(body, &bodymap)
+	if err == nil {
+		host = bodymap.Labels["compute"]
+	}
+	proxyReq, err := http.NewRequest(r.Method, url, bytes.NewBuffer(body))
 	if err != nil {
-		logger.Printf("Sent request from %s failed because %s.", bodymap.Labels["compute"], "create a new request failed")
+		logger.Printf("Sent request from %s failed because %s.", host, "create a new request failed")
 		return
 	}
 	// Filter some headers, otherwise could just use a shallow copy proxyReq.Header = r.Header
@@ -38,10 +42,10 @@ func forwardReq(r *http.Request, url, apiKey string, body []byte, httpClient *ht
 	proxyReq.Header.Add("St2-Api-Key", apiKey)
 	resp, err := httpClient.Do(proxyReq)
 	if err != nil {
-		logger.Printf("Sent request from %s failed because %s.", bodymap.Labels["compute"], "send a POST request failed")
+		logger.Printf("Sent request from %s failed because %s.", host, "send a POST request failed")
 		return
 	}
-	logger.Printf("Sent request from %s successfully.", bodymap.Labels["compute"])
+	logger.Printf("Sent request from %s successfully.", host)
 	defer resp.Body.Close()
 	defer wg.Done()
 	return
