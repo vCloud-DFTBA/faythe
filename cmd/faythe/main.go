1b8a205c0aae588e642f19fea1fda47e91f49e94
Kien Nguyen <kiennt65@viettel.com.vn>
2019-09-10 11:13:40 +0700

    Update cmd

--- cmd/faythe/main.go
+++ cmd/faythe/main.go
@@ -16,8 +16,11 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/go-kit/kit/log/level"
 	"github.com/pkg/errors"
@@ -28,8 +31,11 @@ import (
 
 func main() {
 	cfg := struct {
-		configFile string
-		logConfig  log.Config
+		configFile    string
+		listenAddress string
+		url           string
+		externalURL   *url.URL
+		logConfig     log.Config
 	}{
 		logConfig: log.Config{},
 	}
@@ -38,6 +44,11 @@ func main() {
 	a.HelpFlag.Short('h')
 	a.Flag("config.file", "Faythe configuration file path.").
 		Default("faythe.yml").StringVar(&cfg.configFile)
+	a.Flag("listen-address", "Address to listen on for API.").
+		Default("0.0.0.0:8600").StringVar(&cfg.listenAddress)
+	a.Flag("external-url",
+		"The URL under which Faythe is externally reachable.").
+		PlaceHolder("<URL>").StringVar(&cfg.url)
 
 	logflag.AddFlags(a, &cfg.logConfig)
 	_, err := a.Parse(os.Args[1:])
@@ -48,5 +59,41 @@ func main() {
 	}
 
 	logger := log.New(&cfg.logConfig)
+	cfg.externalURL, err = computeExternalURL(cfg.url, cfg.listenAddress)
 	level.Info(logger).Log("msg", "Staring Faythe")
+}
+
+// A clone of Prometheus computeExternalURL, because it is a internal function:
+// https://github.com/prometheus/prometheus/blob/master/cmd/prometheus/main.go#L791
+func computeExternalURL(u, listenAddr string) (*url.URL, error) {
+	if u == "" {
+		hostname, err := os.Hostname()
+		if err != nil {
+			return nil, err
+		}
+		_, port, err := net.SplitHostPort(listenAddr)
+		if err != nil {
+			return nil, err
+		}
+		u = fmt.Sprintf("http://%s:%s/", hostname, port)
+	}
+
+	// starts or ends with quote
+	if strings.HasPrefix(u, "\"") || strings.HasPrefix(u, "'") ||
+		strings.HasSuffix(u, "\"") || strings.HasSuffix(u, "'") {
+		return nil, errors.New("URL must not begin or end with quotes")
+	}
+
+	eu, err := url.Parse(u)
+	if err != nil {
+		return nil, err
+	}
+
+	ppref := strings.TrimRight(eu.Path, "/")
+	if ppref != "" && !strings.HasPrefix(ppref, "/") {
+		ppref = "/" + ppref
+	}
+	eu.Path = ppref
+
+	return eu, nil
 }
