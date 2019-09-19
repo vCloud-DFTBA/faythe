89f8bd6ad27d60c93931b424447a4ad35c8fe6f0
Kien Nguyen <kiennt65@viettel.com.vn>
2019-09-19 17:05:25 +0700

    Add respond methods

--- api/api.go
+++ api/api.go
@@ -15,12 +15,13 @@
 package api
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"sync"
 	"time"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/gorilla/mux"
 	etcdv3 "go.etcd.io/etcd/clientv3"
 )
@@ -63,31 +64,69 @@ func New(l log.Logger, e *etcdv3.Client) *API {
 
 // Register registers the API handlers under their correct routes
 // in the given router.
-func (api *API) Register(r *mux.Router) {
+func (a *API) Register(r *mux.Router) {
 	wrap := func(f http.HandlerFunc) http.HandlerFunc {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			setCORS(w)
 			f(w, r)
 		})
 	}
-	r.Handle("/", wrap(api.index)).Methods("GET")
-	r.Handle("/status", wrap(api.status)).Methods("GET")
+	r.Handle("/", wrap(a.index)).Methods("GET")
+	r.Handle("/status", wrap(a.status)).Methods("GET")
+	r.Handle("/cloud/{provider}", wrap(a.registerCloud)).Methods("POST")
 }
 
-func (api *API) index(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
-		http.NotFound(w, req)
+func (a *API) receive(req *http.Request, v interface{}) error {
+	dec := json.NewDecoder(req.Body)
+	defer req.Body.Close()
+	err := dec.Decode(v)
+	if err != nil {
+		level.Debug(a.logger).Log("msg", "Decoding request failed", "err", err)
+	}
+	return err
+}
+
+func (a *API) respondError(w http.ResponseWriter, e apiError) {
+	w.Header().Set("Content-Type", "application/json")
+	level.Error(a.logger).Log("msg", "API error", "err", e.Error())
+
+	b, err := json.Marshal(&response{
+		Status: http.StatusText(e.code),
+		Err:    e,
+	})
+
+	if err != nil {
+		level.Error(a.logger).Log("msg", "Error marshalling JSON", "err", err)
+	} else {
+		if _, err := w.Write(b); err != nil {
+			level.Error(a.logger).Log("msg", "failed to write data to connection", "err", err)
+		}
+	}
+
+	http.Error(w, e.Error(), e.code)
+}
+
+type response struct {
+	Status string
+	Data   interface{}
+	Err    apiError
+}
+
+func (a *API) respondSuccess(w http.ResponseWriter, code int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	status := http.StatusText(code)
+	b, err := json.Marshal(&response{
+		Status: status,
+		Data:   data,
+	})
+
+	if err != nil {
+		level.Error(a.logger).Log("msg", "Error marshalling JSON", "err", err)
 		return
 	}
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(w, "Hello stranger! Welcome to Faythe!")
-}
-
-func (api *API) status(w http.ResponseWriter, req *http.Request) {
-	// A placeholder here
-	// Represents Faythe status (version, uptime...) here.
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(w, "Status here - This is just a placeholder")
+	if _, err := w.Write(b); err != nil {
+		level.Error(a.logger).Log("msg", "failed to write data to connection", "err", err)
+	}
 }
