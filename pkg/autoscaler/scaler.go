2ed65a8a40e491595ac706a0f7407037455c8f40
Kien Nguyen <kiennt65@viettel.com.vn>
2019-09-30 18:36:30 +0700

    Revert retry

--- pkg/autoscaler/scaler.go
+++ pkg/autoscaler/scaler.go
@@ -22,7 +22,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/avast/retry-go"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 
@@ -132,43 +131,61 @@ func (s *Scaler) do() {
 	for _, a := range s.Actions {
 		wg.Add(1)
 		go func(url string) {
-			delay, _ := time.ParseDuration(a.Delay)
-			err := retry.Do(
-				func() error {
-					switch a.Type {
-					case "http":
-						// TODO(kiennt): Check kind of action url -> Authen or not?
-						req, err := http.NewRequest(a.Method, url, nil)
-						if err != nil {
-							return err
-						}
-						resp, err := cli.Do(req)
-						if err != nil {
-							return err
-						}
-						level.Info(s.logger).Log("msg", "Sending POST request", "id", s.ID, "url", url)
-						resp.Body.Close()
-					}
-					return nil
-				},
-				retry.DelayType(func(n uint, config *retry.Config) time.Duration {
-					var f retry.DelayTypeFunc
-					switch a.DelayType {
-					case "fixed":
-						f = retry.FixedDelay
-					case "backoff":
-						f = retry.BackOffDelay
-					}
-					return f(n, config)
-				}),
-				retry.Attempts(a.Attempts),
-				retry.Delay(delay),
-			)
+			// TODO(kiennt): Check kind of action url -> Authen or not?
+			req, err := http.NewRequest(a.Method, url, nil)
 			if err != nil {
-				level.Error(s.logger).Log("msg", "Error doing scale action", "url", a.URL.String(), "err", err)
+				level.Error(s.logger).Log("msg", "Error creating request", "url", a.URL, "method", a.Method)
+				return
 			}
+			resp, err := cli.Do(req)
+			if err != nil {
+				level.Error(s.logger).Log("msg", "Error doing request", "url", a.URL, "method", a.Method)
+				return
+			}
+			level.Info(s.logger).Log("msg", "Sending request", "id", s.ID,
+				"url", url, "method", a.Method)
+			resp.Body.Close()
 			defer wg.Done()
 		}(string(a.URL))
+		//go func(url string) {
+		//	delay, _ := time.ParseDuration(a.Delay)
+		//	err := retry.Do(
+		//		func() error {
+		//			// TODO(kiennt): Check kind of action url -> Authen or not?
+		//			req, err := http.NewRequest(a.Method, url, nil)
+		//			if err != nil {
+		//				return err
+		//			}
+		//			resp, err := cli.Do(req)
+		//			if err != nil {
+		//				return err
+		//			}
+		//			level.Info(s.logger).Log("msg", "Sending request", "id", s.ID,
+		//				"url", url, "method", a.Method)
+		//			defer resp.Body.Close()
+		//			return nil
+		//		},
+		//		retry.DelayType(func(n uint, config *retry.Config) time.Duration {
+		//			var f retry.DelayTypeFunc
+		//			switch a.DelayType {
+		//			case "fixed":
+		//				f = retry.FixedDelay
+		//			case "backoff":
+		//				f = retry.BackOffDelay
+		//			}
+		//			return f(n, config)
+		//		}),
+		//		retry.Attempts(a.Attempts),
+		//		retry.Delay(delay),
+		//		retry.RetryIf(func(err error) bool {
+		//			return err != nil
+		//		}),
+		//	)
+		//	if err != nil {
+		//		level.Error(s.logger).Log("msg", "Error doing scale action", "url", a.URL.String(), "err", err)
+		//	}
+		//	defer wg.Done()
+		//}(string(a.URL))
 	}
 	// Wait until all actions were performed
 	wg.Wait()
