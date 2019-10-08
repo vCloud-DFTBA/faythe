bd882d1fee25f980b0b4173b09e2b5266f3d1df6
Kien Nguyen <kiennt65@viettel.com.vn>
2019-10-08 17:17:02 +0700

    Add cluster options

---
+++ pkg/cluster/cluster.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2019 Kien Nguyen-Tuan <kiennt2609@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cluster
+
+import "time"
+
+// Options for the cluster handler
+type Options struct {
+	BindAddr         string
+	AdvertiseAddr    string
+	Peers            string
+	PeerTimeout      time.Duration
+	GossipInterval   time.Duration
+	TCPTimeout       time.Duration
+	ProbeTimeout     time.Duration
+	ProbeInterval    time.Duration
+	PushPullInterval time.Duration
+}
+
+const (
+	DefaultGossipInterval   = 200 * time.Millisecond
+	DefaultTCPTimeout       = 10 * time.Second
+	DefaultProbeTimeout     = 500 * time.Millisecond
+	DefaultProbeInterval    = 1 * time.Second
+	DefaultPushPullInterval = 30 * time.Second
+)
