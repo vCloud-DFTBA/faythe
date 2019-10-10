ad0630cd59193560da77e112dceb082ed609e41d
Kien Nguyen <kiennt65@viettel.com.vn>
2019-10-10 11:30:39 +0700

    Init Peer struct

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -14,7 +14,36 @@
 
 package cluster
 
-import "time"
+import (
+	"crypto"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/hashicorp/memberlist"
+	"github.com/ntk148v/faythe/pkg/utils"
+	"github.com/pkg/errors"
+)
+
+const (
+	DefaultGossipInterval   = 200 * time.Millisecond
+	DefaultTCPTimeout       = 10 * time.Second
+	DefaultProbeTimeout     = 500 * time.Millisecond
+	DefaultProbeInterval    = 1 * time.Second
+	DefaultPushPullInterval = 30 * time.Second
+)
+
+type logWriter struct {
+	l log.Logger
+}
+
+func (l *logWriter) Write(b []byte) (int, error) {
+	return len(b), level.Debug(l.l).Log("memberlist", string(b))
+}
 
 // Options for the cluster handler
 type Options struct {
@@ -29,10 +58,98 @@ type Options struct {
 	PushPullInterval time.Duration
 }
 
-const (
-	DefaultGossipInterval   = 200 * time.Millisecond
-	DefaultTCPTimeout       = 10 * time.Second
-	DefaultProbeTimeout     = 500 * time.Millisecond
-	DefaultProbeInterval    = 1 * time.Second
-	DefaultPushPullInterval = 30 * time.Second
-)
+// Peer is a single peer in a gossip cluster.
+type Peer struct {
+	mtx    sync.RWMutex
+	logger log.Logger
+	opts   *Options
+	mlist  *memberlist.Memberlist
+}
+
+// Clone from Prometheus alertmanager
+// https://github.com/prometheus/alertmanager/blob/master/cluster/cluster.go
+// Create creates a new peer.
+func Create(l log.Logger, o *Options) (*Peer, error) {
+	bindHost, bindPortStr, err := net.SplitHostPort(o.BindAddr)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid listen address")
+	}
+	bindPort, err := strconv.Atoi(bindPortStr)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid listen address, wrong port")
+	}
+	var (
+		advertiseHost string
+		advertisePort int
+	)
+
+	if o.AdvertiseAddr != "" {
+		var advertisePortStr string
+		advertiseHost, advertisePortStr, err = net.SplitHostPort(o.AdvertiseAddr)
+		if err != nil {
+			return nil, errors.Wrap(err, "invalid advertise address")
+		}
+		advertisePort, err = strconv.Atoi(advertisePortStr)
+		if err != nil {
+			return nil, errors.Wrap(err, "invalid advertise address, wrong port")
+		}
+	}
+
+	// Initial validation of user-specified advertise address.
+	addr, err := findAdvertiseAddress(bindHost, advertiseHost)
+	if err != nil {
+		level.Warn(l).Log("err", "couldn't deduce an advertise address: "+err.Error())
+	} else if isUnroutable(addr.String()) {
+		level.Warn(l).Log("err", "this node advertise itself on an unroutable address", "addr", addr.String())
+		level.Warn(l).Log("err", "this node will be unreachable in the cluster")
+		level.Warn(l).Log("err", "provide --cluster.advertise-address as a routable IP address or hostname")
+	} else if isAny(o.BindAddr) && advertiseHost == "" {
+		// memberlist doesn't advertise properly when the bind address is empty or unspecified.
+		level.Info(l).Log("msg", "setting advertise address explicitly", "addr", addr.String(), "port", bindPort)
+		advertiseHost = addr.String()
+		advertisePort = bindPort
+	}
+	p := &Peer{
+		logger: l,
+		opts:   o,
+	}
+	cfg := memberlist.DefaultLANConfig()
+	cfg.Name = string(utils.Hash(o.BindAddr, crypto.MD5))
+	cfg.BindAddr = bindHost
+	cfg.BindPort = bindPort
+	cfg.GossipInterval = o.GossipInterval
+	cfg.ProbeTimeout = o.ProbeTimeout
+	cfg.ProbeInterval = o.ProbeInterval
+	cfg.PushPullInterval = o.PushPullInterval
+	cfg.TCPTimeout = o.TCPTimeout
+	cfg.LogOutput = &logWriter{l: l}
+	ml, err := memberlist.Create(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "create memberlist")
+	}
+	if advertiseHost != "" {
+		cfg.AdvertiseAddr = advertiseHost
+		cfg.AdvertisePort = advertisePort
+	}
+	p.mlist = ml
+	return p, nil
+}
+
+func isUnroutable(addr string) bool {
+	if host, _, err := net.SplitHostPort(addr); err == nil {
+		addr = host
+	}
+	if ip := net.ParseIP(addr); ip != nil && (ip.IsUnspecified() || ip.IsLoopback()) {
+		return true // typically 0.0.0.0 or localhost
+	} else if ip == nil && strings.ToLower(addr) == "localhost" {
+		return true
+	}
+	return false
+}
+
+func isAny(addr string) bool {
+	if host, _, err := net.SplitHostPort(addr); err == nil {
+		addr = host
+	}
+	return addr == "" || net.ParseIP(addr).IsUnspecified()
+}
