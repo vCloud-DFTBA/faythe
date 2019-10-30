a2d2e8e4238a4333d9664f623e03a5ada0801d23
Kien Nguyen <kiennt65@viettel.com.vn>
2019-10-30 10:41:26 +0700

    Fix import

--- cmd/faythe/main.go
+++ cmd/faythe/main.go
@@ -16,7 +16,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/ntk148v/faythe/pkg/cluster"
 	"net"
 	"net/http"
 	"net/url"
@@ -43,6 +42,7 @@ import (
 	"github.com/ntk148v/faythe/config"
 	"github.com/ntk148v/faythe/middleware"
 	"github.com/ntk148v/faythe/pkg/autoscaler"
+	"github.com/ntk148v/faythe/pkg/cluster"
 )
 
 func main() {
