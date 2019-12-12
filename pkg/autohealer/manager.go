b65dd4f9cec20e1597b72010e49d91d8f703fd48
Vu Tuan Dat <datvt18@viettel.com.vn>
2019-12-12 16:51:52 +0700

    Rearrange import

--- pkg/autohealer/manager.go
+++ pkg/autohealer/manager.go
@@ -19,15 +19,15 @@ import (
 	"crypto"
 	"encoding/json"
 	"fmt"
+	"strings"
+	"sync"
+
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	etcdv3 "go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/mvcc/mvccpb"
-	"strings"
-	"sync"
 
 	"github.com/vCloud-DFTBA/faythe/pkg/cluster"
-
 	"github.com/vCloud-DFTBA/faythe/pkg/common"
 	"github.com/vCloud-DFTBA/faythe/pkg/exporter"
 	"github.com/vCloud-DFTBA/faythe/pkg/metrics"
