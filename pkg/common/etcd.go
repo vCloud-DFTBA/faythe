5c62863b836136cd3912759702955244d6fe9b0c
Kien Nguyen <kiennt65@viettel.com.vn>
2020-10-02 13:48:03 +0700

    Fix import order

--- pkg/common/etcd.go
+++ pkg/common/etcd.go
@@ -16,13 +16,13 @@ package common
 
 import (
 	"context"
-	"github.com/pkg/errors"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	etcdv3 "go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/clientv3/namespace"
