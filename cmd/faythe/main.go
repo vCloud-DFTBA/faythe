0f03b34b5aeabfbc86ad5e8155393cc20b5ad50d
Vu Tuan Dat <datvt18@viettel.com.vn>
2022-08-22 15:41:58 +0700

    Make faythe TLS compatible

--- cmd/faythe/main.go
+++ cmd/faythe/main.go
@@ -15,8 +15,8 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
-	"github.com/vCloud-DFTBA/faythe/pkg/scheduler"
 	"net"
 	"net/http"
 	"net/url"
@@ -49,6 +49,7 @@ import (
 	"github.com/vCloud-DFTBA/faythe/pkg/cloud/store/openstack"
 	"github.com/vCloud-DFTBA/faythe/pkg/cluster"
 	"github.com/vCloud-DFTBA/faythe/pkg/common"
+	"github.com/vCloud-DFTBA/faythe/pkg/scheduler"
 )
 
 func init() {
@@ -196,7 +197,27 @@ func main() {
 	defer stopFunc()
 
 	// Init HTTP server
-	srv := http.Server{Addr: cfg.listenAddress, Handler: router}
+	serverConfig := config.Get().ServerConfig
+	tlscfg := &tls.Config{
+		MinVersion:       tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	srv := &http.Server{
+		Addr:           cfg.listenAddress,
+		ReadTimeout:    serverConfig.ReadTimeout,
+		WriteTimeout:   serverConfig.WriteTimeout,
+		MaxHeaderBytes: serverConfig.MaxHeaderBytes,
+		TLSConfig:      tlscfg,
+		Handler:        router,
+		TLSNextProto:   make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
 	srvc := make(chan struct{})
 
 	go func() {
@@ -216,9 +237,17 @@ func main() {
 
 	go func() {
 		level.Info(logger).Log("msg", "Listening", "address", cfg.listenAddress)
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			level.Error(logger).Log("msg", "Listen error", "err", err)
-			close(srvc)
+		switch serverConfig.EnableTLS {
+		case true:
+			if err := srv.ListenAndServeTLS(serverConfig.CertFile, serverConfig.CertKey); err != nil {
+				level.Error(logger).Log("msg", "Listen error", "err", err)
+				close(srvc)
+			}
+		default:
+			if err := srv.ListenAndServe(); err != nil {
+				level.Error(logger).Log("msg", "Listen error", "err", err)
+				close(srvc)
+			}
 		}
 		defer func() {
 			if err := srv.Close(); err != nil {
